4ce2c246889226bdbe38b4e50b34f4c70d42dcfb
Salvador Martínez <bryanloopxz@gmail.com>
2024-11-15 08:22:51 -0600

    lol

--- src/Fragments/Navigation.jsx
+++ src/Fragments/Navigation.jsx
@@ -5,10 +5,12 @@ import { faUser, faNetworkWired } from '@fortawesome/free-solid-svg-icons'
 
 function Navigation() {
   return (
-    <nav className="fixed top-0 w-full h-16 flex items-center justify-between px-8">
-      <a href="#"><FontAwesomeIcon icon={faNetworkWired} size="2x" /></a>
-      <a href="#"><FontAwesomeIcon icon={faUser} size="2x" /></a>
-    </nav>
+    <div className="nav">
+      <nav className="fixed top-0 w-full h-16 flex items-center justify-between px-8">
+        <a href="#"><FontAwesomeIcon icon={faNetworkWired} size="2x" /></a>
+        <a href="#"><FontAwesomeIcon icon={faUser} size="2x" /></a>
+      </nav>
+    </div>
   );
 }
 
