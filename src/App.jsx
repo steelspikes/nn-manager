2af28ba8959ddd50a94acf25ff4202f364a670a0
Salvador Martínez <bryanloopxz@gmail.com>
2024-11-15 08:18:10 -0600

    Pantalla de modelos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './Screens/Login';
 import Navigation from './Fragments/Navigation';
+import './Styles/styles.css';
+import Projects from './Screens/Projects';
+import PerceptronPlot from './Fragments/PerceptronPlot';
 
 function App() {
   return (
